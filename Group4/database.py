c2e5cb0843533d1311e2ca4f1a65e7252f8babdc
Anastasia Tyo <208531634+Asty-brmn@users.noreply.github.com>
2026-03-22 19:52:01 +0500

    Fix: explicitly export DATABASE_URL before uvicorn

--- Group4/database.py
+++ Group4/database.py
@@ -12,9 +12,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.sql import func
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/Messages"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/Messages"
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:12345@localhost/Messages")
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:12345@localhost/Messages"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
